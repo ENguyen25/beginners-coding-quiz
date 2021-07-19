ed4274c111512b9fa7deca4a922c4f2e2aa155b0
Evelyn Nguyen <enguyen6208@gmail.com>
2021-07-19 16:45:48 -0700

    added function for timer and inputting initials but only the timer function works

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // list of variables
 var startButton = document.querySelector('.start-button');
+var timer = document.querySelector('.timer');
+var inputInitials = document.getElementById('#student-initials');
 var questions = document.querySelector('.question');
 var li1 = document.querySelector('.response-1');
 var li2 = document.querySelector('.response-2');
 var li3 = document.querySelector('.response-3');
 var li4 = document.querySelector('.response-4');
+var secondsLeft = 11;
 var myQuestions =  [
     {
         question: "What is 10+10",
@@ -46,7 +49,33 @@ function openQuestions() {
 }
 
 // total correct responses
+function startQuiz() {
+    
+}
 
 // timer
+function countdown() {
+
+    var timeInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = 'Time: ' + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timeInterval);
+        }
+
+    }, 1000);
+
+}
+
+countdown();
 
 // input initials
+function saveInitials() {
+    console.log(inputInitials)
+    var studentInitials = {
+        initials: inputInitials.value};
+    localStorage.setItem('studentInitials', JSON.stringify(studentInitials));
+
+}
+saveInitials();

62da3aefdfab5377179ff77644dd2f1cf657eab8
Evelyn Nguyen <enguyen6208@gmail.com>
2021-07-19 19:08:27 -0700

    added function that listens for a response and checks if the response is correct or incorrect

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // list of variables
 var startButton = document.querySelector('.start-button');
 var timer = document.querySelector('.timer');
-var inputInitials = document.getElementById('#student-initials');
+var inputInitials = document.querySelector('#student-initials');
+var listOfResponses = document.querySelector('.responses');
+var submitButton = document.querySelector('.submit-button');
 var questions = document.querySelector('.question');
+var currentQuestion = 0;
+var numOfCorrectResponses = 0;
 var li1 = document.querySelector('.response-1');
 var li2 = document.querySelector('.response-2');
 var li3 = document.querySelector('.response-3');
@@ -15,20 +19,23 @@ var myQuestions =  [
         b: 24,
         c: 30,
         d: 21,
+        correctAnswer: 20,
     },
     {
         question: "What is 25/5",
         a: 20,
-        b: 24,
+        b: 5,
         c: 30,
         d: 21,
+        correctAnswer: 5,
     },
     {
         question: "What is 40+2",
         a: 20,
         b: 24,
         c: 30,
-        d: 21,
+        d: 42,
+        correctAnswer: 42,
     }
 ];
 
@@ -37,21 +44,32 @@ var number = 1;
 // button to start quiz
 startButton.addEventListener('click', openQuestions);
 
+i = 0;
 // pop up questions
 function openQuestions() {
     for (i = 0; i < myQuestions.length; i++) {
+        currentQuestion = i
         questions.textContent = myQuestions[i].question;
         li1.textContent = myQuestions[i].a;
         li2.textContent = myQuestions[i].b;
         li3.textContent = myQuestions[i].c;
         li4.textContent = myQuestions[i].d;
-    }
-}
+
+        listOfResponses.addEventListener('click', function(event) {
+            var response = event.target.textContent;
+
+            if (response == myQuestions[currentQuestion].correctAnswer) {
+                numOfCorrectResponses++;
+                openQuestions();
+            } else {
+                openQuestions();
+            }
+        }   
+)}};
+    
 
 // total correct responses
-function startQuiz() {
-    
-}
+
 
 // timer
 function countdown() {
@@ -71,11 +89,11 @@ function countdown() {
 countdown();
 
 // input initials
-function saveInitials() {
-    console.log(inputInitials)
+function saveInitials(event) {
+    event.preventDefault();
     var studentInitials = {
         initials: inputInitials.value};
     localStorage.setItem('studentInitials', JSON.stringify(studentInitials));
 
 }
-saveInitials();
+submitButton.addEventListener("click", saveInitials);
